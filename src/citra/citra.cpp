41b6ae04e4bdc7807379f0f92cbbae0a931c433b
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2017-10-04 10:42:02 -0500

    Update citra.cpp

--- src/citra/citra.cpp
+++ src/citra/citra.cpp
@@ -48,7 +48,7 @@ static void PrintHelp(const char* argv0) {
 }
 
 static void PrintVersion() {
-    std::cout << "Citra Canary " << "tomodachi" << " " << Common::g_scm_desc << std::endl;
+    std::cout << "Citra Nightly " << "tomodachi" << " " << Common::g_scm_desc << std::endl;
 }
 
 /// Application entry point
