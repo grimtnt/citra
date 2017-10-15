161a852b7190d813849c6a51478d1131b3db1e05
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2017-10-15 00:21:54 -0500

    Travis (#101)
    * Create .travis.yml
    
    * Create script.sh
    
    * Create post-upload.sh
    
    * Create pre-upload.sh
    
    * Create build.sh
    
    * Create deps.sh
    
    * Create docker.sh
    
    * Create upload.sh
    
    * Create build.sh
    
    * Create deps.sh
    
    * Create upload.sh

---
+++ .travis/linux/upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+. .travis/common/pre-upload.sh
+
+REV_NAME="citra-linux-${GITDATE}-${GITREV}"
+ARCHIVE_NAME="${REV_NAME}.tar.xz"
+COMPRESSION_FLAGS="-cJvf"
+
+mkdir "$REV_NAME"
+
+cp build/src/citra/citra "$REV_NAME"
+cp build/src/citra_qt/citra-qt "$REV_NAME"
+
+# We need icons on Linux for .desktop entries
+mkdir "$REV_NAME/dist"
+cp dist/icon.png "$REV_NAME/dist/citra.png"
+
+. .travis/common/post-upload.sh
