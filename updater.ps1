7bb18e786d0004cc6c7b0a06189097df4e0a9bed
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2017-11-22 13:24:03 -0600

    updater [skip ci]

---
+++ updater.ps1
@@ -0,0 +1,14 @@
+$releases = "https://api.github.com/repos/acnleditor2/citra/releases"
+$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+$assets = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets_url
+$file = (Invoke-WebRequest $assets | ConvertFrom-Json)[0].name
+$download = "https://github.com/acnleditor2/citra/releases/download/$tag/$file"
+$name = $file.Split(".")[0]
+$dir = "$name"
+Invoke-WebRequest $download -Out "citra.zip"
+Expand-Archive "citra.zip"
+Move-Item "citra/head-mingw/*" ".\"
+Remove-Item "citra/head-mingw" -Recurse
+Remove-Item "citra"
+Remove-Item "citra.zip"
+start "citra-qt.exe"
