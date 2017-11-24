f90ee7c70ab120f89d3dcdec54b46e439de21e5f
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2017-11-24 10:52:23 -0600

    [skip ci]

---
+++ src/core/country.h
@@ -0,0 +1,12 @@
+// Copyright 2014 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <string>
+
+namespace Country {
+
+std::string GetName(int id);
+} // namespace Country
