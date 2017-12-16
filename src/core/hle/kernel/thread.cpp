575587d0c27e2ab6572e0b7e8eda5a18433c88a4
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2017-12-16 13:25:42 -0600

    Nintendogs + Cats

--- src/core/hle/kernel/thread.cpp
+++ src/core/hle/kernel/thread.cpp
@@ -99,28 +99,6 @@ void Thread::Stop() {
     Kernel::g_current_process->tls_slots[tls_page].reset(tls_slot);
 }
 
-    /// Boost low priority threads (temporarily) that have been starved
-    static void PriorityBoostStarvedThreads() {
-    u64 current_ticks = CoreTiming::GetTicks();
-
-    for (auto& thread : thread_list) {
-        // TODO(bunnei): Threads that have been waiting to be scheduled for `boost_ticks` (or
-        // longer) will have their priority temporarily adjusted to 1 higher than the highest
-        // priority thread to prevent thread starvation. This general behavior has been verified
-        // on hardware. However, this is almost certainly not perfect, and the real CTR OS scheduler
-        // should probably be reversed to verify this.
-
-        const u64 boost_timeout = 2000000; // Boost threads that have been ready for > this long
-
-        u64 delta = current_ticks - thread->last_running_ticks;
-
-        if (thread->status == THREADSTATUS_READY && delta > boost_timeout) {
-            const s32 priority = (ready_queue.get_first()->current_priority - 1, 0);
-            thread->BoostPriority(priority);
-        }
-    }
-}
-    
 /**
  * Switches the CPU's active thread context to that of the specified thread
  * @param new_thread The thread to switch to
@@ -488,9 +466,7 @@ bool HaveReadyThreads() {
     return ready_queue.get_first() != nullptr;
 }
 
-void Reschedule() {
-    PriorityBoostStarvedThreads();
-    
+void Reschedule() {    
     Thread* cur = GetCurrentThread();
     Thread* next = PopNextReadyThread();
 
