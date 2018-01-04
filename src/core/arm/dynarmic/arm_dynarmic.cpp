889dce1afcc49a0482d65083283ca5fc25963ded
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-01-03 23:26:25 -0600

    [skip ci]

--- src/core/arm/dynarmic/arm_dynarmic.cpp
+++ src/core/arm/dynarmic/arm_dynarmic.cpp
@@ -95,7 +95,7 @@ static bool IsReadOnlyMemory(u32 vaddr) {
 }
 
 static void AddTicks(u64 ticks) {
-    CoreTiming::AddTicks(10900);
+    CoreTiming::AddTicks(10500);
 }
 
 static u64 GetTicksRemaining() {
