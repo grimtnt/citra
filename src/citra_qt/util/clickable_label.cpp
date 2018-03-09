b436addcc622599773f28654b0eec33f94c70a69
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-03-09 12:54:10 -0600

    [skip ci]

--- src/citra_qt/util/clickable_label.cpp
+++ src/citra_qt/util/clickable_label.cpp
@@ -1,4 +1,8 @@
-#include "util/clickable_label.h"
+// Copyright 2017 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include "citra_qt/util/clickable_label.h"
 
 ClickableLabel::ClickableLabel(QWidget* parent, Qt::WindowFlags f) : QLabel(parent) {}
 
