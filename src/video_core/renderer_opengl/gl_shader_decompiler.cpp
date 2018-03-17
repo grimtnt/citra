26921db8158060777462d97b918c4b477deb612a
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-03-17 09:34:13 -0600

    glvtx update

--- src/video_core/renderer_opengl/gl_shader_decompiler.cpp
+++ src/video_core/renderer_opengl/gl_shader_decompiler.cpp
@@ -3,9 +3,9 @@
 // Refer to the license.txt file included.
 
 #include <map>
-#include <optional>
 #include <set>
 #include <string>
+#include <boost/optional.hpp>
 #include <nihstro/shader_bytecode.h>
 #include <queue>
 #include "common/assert.h"
@@ -105,16 +105,16 @@ private:
      * @param begin the code starting point.
      * @end the farest point to search for "end" instructions.
      * @return the total number instructions covered. If there is a code path not reaching an "end"
-     *     instruction, the return value is std::nullopt.
+     *     instruction, the return value is boost::none.
      */
-    std::optional<size_t> FindEndInstr(u32 begin, u32 end) {
+    boost::optional<size_t> FindEndInstr(u32 begin, u32 end) {
         // first: offset
         // bool: found END
         std::map<u32, bool> checked_offsets;
         if (FindEndInstrImpl(begin, end, checked_offsets)) {
             return checked_offsets.size();
         } else {
-            return std::nullopt;
+            return boost::none;
         }
     }
 
@@ -160,7 +160,7 @@ private:
         u32 end;
 
         std::set<std::pair<u32, u32>> discovered;
-        std::optional<size_t> end_instr_distance;
+        boost::optional<size_t> end_instr_distance;
 
         using SubroutineMap = std::map<std::pair<u32 /*begin*/, u32 /*end*/>, const Subroutine*>;
         SubroutineMap branches;
@@ -311,6 +311,62 @@ private:
         return false;
     }
 
+    /// Generates condition evaluation code for the flow control instruction.
+    static std::string EvaluateCondition(Instruction::FlowControlType flow_control) {
+        using Op = Instruction::FlowControlType::Op;
+
+        std::string result_x =
+            flow_control.refx.Value() ? "conditional_code.x" : "!conditional_code.x";
+        std::string result_y =
+            flow_control.refy.Value() ? "conditional_code.y" : "!conditional_code.y";
+
+        switch (flow_control.op) {
+        case Op::JustX:
+            return result_x;
+        case Op::JustY:
+            return result_y;
+        case Op::Or:
+        case Op::And: {
+            std::string and_or = flow_control.op == Op::Or ? "any" : "all";
+            std::string bvec;
+            if (flow_control.refx.Value() && flow_control.refy.Value()) {
+                bvec = "conditional_code";
+            } else if (!flow_control.refx.Value() && !flow_control.refy.Value()) {
+                bvec = "not(conditional_code)";
+            } else {
+                bvec = "bvec2(" + result_x + ", " + result_y + ")";
+            }
+            return and_or + "(" + bvec + ")";
+        }
+        default:
+            UNREACHABLE();
+            return "";
+        }
+    };
+
+    /// Generates code representing the source register.
+    std::string GetSourceRegister(const SourceRegister& source_reg,
+                                  u32 address_register_index) const {
+        u32 index = static_cast<u32>(source_reg.GetIndex());
+        std::string index_str = std::to_string(index);
+
+        switch (source_reg.GetRegisterType()) {
+        case RegisterType::Input:
+            return inputreg_getter(index);
+        case RegisterType::Temporary:
+            return "reg_tmp" + index_str;
+        case RegisterType::FloatUniform:
+            if (address_register_index != 0) {
+                index_str +=
+                    std::string(" + address_registers.") + "xyz"[address_register_index - 1];
+            }
+            return "uniforms.f[" + index_str + "]";
+        default:
+            UNREACHABLE();
+            return "";
+        }
+    };
+
 public:
     Impl(const std::array<u32, MAX_PROGRAM_CODE_LENGTH>& program_code,
          const std::array<u32, MAX_SWIZZLE_DATA_LENGTH>& swizzle_data, u32 main_offset,
@@ -367,60 +423,6 @@ public:
             }
         }
         add_line("");
-
-        auto evaluate_condition = [](Instruction::FlowControlType flow_control) -> std::string {
-            using Op = Instruction::FlowControlType::Op;
-
-            std::string result_x =
-                flow_control.refx.Value() ? "conditional_code.x" : "!conditional_code.x";
-            std::string result_y =
-                flow_control.refy.Value() ? "conditional_code.y" : "!conditional_code.y";
-
-            switch (flow_control.op) {
-            case Op::JustX:
-                return result_x;
-            case Op::JustY:
-                return result_y;
-            case Op::Or:
-            case Op::And: {
-                std::string and_or = flow_control.op == Op::Or ? "any" : "all";
-                std::string bvec;
-                if (flow_control.refx.Value() && flow_control.refy.Value()) {
-                    bvec = "conditional_code";
-                } else if (!flow_control.refx.Value() && !flow_control.refy.Value()) {
-                    bvec = "not(conditional_code)";
-                } else {
-                    bvec = "bvec2(" + result_x + ", " + result_y + ")";
-                }
-                return and_or + "(" + bvec + ")";
-            }
-            default:
-                UNREACHABLE();
-                return "";
-            }
-        };
-
-        auto get_source_register = [this](const SourceRegister& source_reg,
-                                          u32 address_register_index) -> std::string {
-            u32 index = static_cast<u32>(source_reg.GetIndex());
-            std::string index_str = std::to_string(index);
-
-            switch (source_reg.GetRegisterType()) {
-            case RegisterType::Input:
-                return inputreg_getter(index);
-            case RegisterType::Temporary:
-                return "reg_tmp" + index_str;
-            case RegisterType::FloatUniform:
-                if (address_register_index != 0) {
-                    index_str +=
-                        std::string(" + address_registers.") + "xyz"[address_register_index - 1];
-                }
-                return "uniforms.f[" + index_str + "]";
-            default:
-                UNREACHABLE();
-                return "";
-            }
-        };
 
         auto selector_to_string = [](const auto& selector_getter) -> std::string {
             std::string out;
@@ -507,14 +509,14 @@ public:
                     (0 != (instr.opcode.Value().GetInfo().subtype & OpCode::Info::SrcInversed));
 
                 std::string src1 = swizzle.negate_src1 ? "-" : "";
-                src1 += get_source_register(instr.common.GetSrc1(is_inverted),
-                                            !is_inverted * instr.common.address_register_index);
+                src1 += GetSourceRegister(instr.common.GetSrc1(is_inverted),
+                                          !is_inverted * instr.common.address_register_index);
                 src1 += "." +
                         selector_to_string([&](int comp) { return swizzle.GetSelectorSrc1(comp); });
 
                 std::string src2 = swizzle.negate_src2 ? "-" : "";
-                src2 += get_source_register(instr.common.GetSrc2(is_inverted),
-                                            is_inverted * instr.common.address_register_index);
+                src2 += GetSourceRegister(instr.common.GetSrc2(is_inverted),
+                                          is_inverted * instr.common.address_register_index);
                 src2 += "." +
                         selector_to_string([&](int comp) { return swizzle.GetSelectorSrc2(comp); });
 
@@ -683,19 +685,19 @@ public:
                     bool is_inverted = (instr.opcode.Value().EffectiveOpCode() == OpCode::Id::MADI);
 
                     std::string src1 = swizzle.negate_src1 ? "-" : "";
-                    src1 += get_source_register(instr.mad.GetSrc1(is_inverted), 0);
+                    src1 += GetSourceRegister(instr.mad.GetSrc1(is_inverted), 0);
                     src1 += "." + selector_to_string(
                                       [&](int comp) { return swizzle.GetSelectorSrc1(comp); });
 
                     std::string src2 = swizzle.negate_src2 ? "-" : "";
-                    src2 += get_source_register(instr.mad.GetSrc2(is_inverted),
-                                                !is_inverted * instr.mad.address_register_index);
+                    src2 += GetSourceRegister(instr.mad.GetSrc2(is_inverted),
+                                              !is_inverted * instr.mad.address_register_index);
                     src2 += "." + selector_to_string(
                                       [&](int comp) { return swizzle.GetSelectorSrc2(comp); });
 
                     std::string src3 = swizzle.negate_src3 ? "-" : "";
-                    src3 += get_source_register(instr.mad.GetSrc3(is_inverted),
-                                                is_inverted * instr.mad.address_register_index);
+                    src3 += GetSourceRegister(instr.mad.GetSrc3(is_inverted),
+                                              is_inverted * instr.mad.address_register_index);
                     src3 += "." + selector_to_string(
                                       [&](int comp) { return swizzle.GetSelectorSrc3(comp); });
 
@@ -732,7 +734,7 @@ public:
                 case OpCode::Id::JMPU: {
                     std::string condition;
                     if (instr.opcode.Value() == OpCode::Id::JMPC) {
-                        condition = evaluate_condition(instr.flow_control);
+                        condition = EvaluateCondition(instr.flow_control);
                     } else {
                         bool invert_test = instr.flow_control.num_instructions & 1;
                         condition = (invert_test ? "!" : "") +
@@ -753,7 +755,7 @@ public:
                 case OpCode::Id::CALLU: {
                     std::string condition;
                     if (instr.opcode.Value() == OpCode::Id::CALLC) {
-                        condition = evaluate_condition(instr.flow_control);
+                        condition = EvaluateCondition(instr.flow_control);
                     } else if (instr.opcode.Value() == OpCode::Id::CALLU) {
                         condition = get_uniform_bool(instr.flow_control.bool_uniform_id);
                     }
@@ -783,7 +785,7 @@ public:
                 case OpCode::Id::IFU: {
                     std::string condition;
                     if (instr.opcode.Value() == OpCode::Id::IFC) {
-                        condition = evaluate_condition(instr.flow_control);
+                        condition = EvaluateCondition(instr.flow_control);
                     } else {
                         condition = get_uniform_bool(instr.flow_control.bool_uniform_id);
                     }
@@ -906,7 +908,7 @@ public:
                         return true;
                     }
                 }
-                return subroutine.end_instr_distance.has_value();
+                return subroutine.end_instr_distance.is_initialized();
             };
 
             if (subroutine.end_instr_distance) {
