b43e96ff3160f9628c5134489426a1f29ab250fa
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-03-23 22:22:32 -0600

    glvtx update

--- src/video_core/renderer_opengl/gl_shader_decompiler.h
+++ src/video_core/renderer_opengl/gl_shader_decompiler.h
@@ -12,15 +12,15 @@ namespace Pica {
 namespace Shader {
 namespace Decompiler {
 
+using ProgramCode = std::array<u32, MAX_PROGRAM_CODE_LENGTH>;
+using SwizzleData = std::array<u32, MAX_SWIZZLE_DATA_LENGTH>;
+using RegGetter = std::function<std::string(u32)>;
+
 std::string GetCommonDeclarations();
 
-std::string DecompileProgram(const std::array<u32, MAX_PROGRAM_CODE_LENGTH>& program_code,
-                             const std::array<u32, MAX_SWIZZLE_DATA_LENGTH>& swizzle_data,
-                             u32 main_offset,
-                             const std::function<std::string(u32)>& inputreg_getter,
-                             const std::function<std::string(u32)>& outputreg_getter,
-                             bool sanitize_mul, const std::string& emit_cb = "",
-                             const std::string& setemit_cb = "");
+std::string DecompileProgram(const ProgramCode& program_code, const SwizzleData& swizzle_data,
+                             u32 main_offset, const RegGetter& inputreg_getter,
+                             const RegGetter& outputreg_getter, bool sanitize_mul, bool is_gs);
 
 } // namespace Decompiler
 } // namespace Shader
