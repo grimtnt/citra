2ab80799e4ada05ec9d7c4d30c81ba992158c0a8
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-03-27 12:58:02 -0600

    [skip ci]

--- src/common/logging/log.h
+++ src/common/logging/log.h
@@ -62,6 +62,7 @@ enum class Class : ClassType {
     Service_AM,        ///< The AM (Application manager) service
     Service_PTM,       ///< The PTM (Power status & misc.) service
     Service_LDR,       ///< The LDR (3ds dll loader) service
+    Service_MCU,       ///< The MCU (Auxiliary Microcontroller) service
     Service_MIC,       ///< The MIC (Microphone) service
     Service_NDM,       ///< The NDM (Network daemon manager) service
     Service_NFC,       ///< The NFC service
