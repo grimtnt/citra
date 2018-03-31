c491cac3f991d7ea57488f6bcff956a2edf32896
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-03-31 09:58:10 -0600

    [skip ci]

--- src/citra_qt/camera/opencv_camera.cpp
+++ src/citra_qt/camera/opencv_camera.cpp
@@ -118,7 +118,7 @@ bool OpenCVCamera::CanReceiveFrame() {
     return ret;
 }
 
-std::unique_ptr<CameraInterface> OpenCVCameraFactory::Create(const std::string& config) const {
+std::unique_ptr<CameraInterface> OpenCVCameraFactory::Create(const std::string& config) {
     return std::make_unique<OpenCVCamera>(config);
 }
 
