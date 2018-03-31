819a799a44cf503ab809bc3656f2051dc3b9f73f
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-03-30 23:42:40 -0600

    [skip ci]

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -52,15 +52,15 @@
 #include "core/settings.h"
 #include "video_core/video_core.h"
 
+#ifdef QT_STATICPLUGIN
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+#endif
+
 #ifdef _WIN32
 extern "C" {
 // tells Nvidia drivers to use the dedicated GPU by default on laptops with switchable graphics
 __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
 }
-#endif
-
-#ifdef QT_STATICPLUGIN
-Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 #endif
 
 /**
