7930ab999d084f991a42478341c273e42ce8f56f
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-04 10:12:52 -0500

    [skip ci]

--- src/citra_qt/configuration/configure_camera.h
+++ src/citra_qt/configuration/configure_camera.h
@@ -24,28 +24,29 @@ public:
     void timerEvent(QTimerEvent*) override;
 
 public slots:
-    void onCameraChanged(int index);
-    void onCameraConfigChanged(const QString& text);
+    void onCameraChanged();
     void onImageSourceChanged(int index);
-    void onImplementationChanged(const QString& text);
     void onToolButtonClicked();
     void onPromptBeforeLoadChanged(int state);
 
 private:
+    enum class CameraPosition;
     void setConfiguration();
     void setUiDisplay();
     void startPreviewing();
     void stopPreviewing();
     void connectEvents();
+    CameraPosition getCameraSelection();
+    int getSelectedCameraIndex();
 
 private:
     std::unique_ptr<Ui::ConfigureCamera> ui;
     std::array<std::string, 3> camera_name;
     std::array<std::string, 3> camera_config;
-    int camera_selection = 0;
     int timer_id = 0;
     int preview_width = 0;
     int preview_height = 0;
+    CameraPosition current_selected;
     bool is_previewing = false;
     std::unique_ptr<Camera::CameraInterface> previewing_camera;
 };
