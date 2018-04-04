848cb872f7d7b3bc995734c491b11e86fcbe1344
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-03 21:22:31 -0500

    citra_qt: camera integration fixes

--- src/citra_qt/camera/still_image_camera.h
+++ src/citra_qt/camera/still_image_camera.h
@@ -21,7 +21,7 @@ public:
     void SetFlip(Service::CAM::Flip) override;
     void SetEffect(Service::CAM::Effect) override;
     void SetFormat(Service::CAM::OutputFormat) override;
-    void SetFrameRate(Service::CAM::FrameRate frame_rate) override{};
+    void SetFrameRate(Service::CAM::FrameRate frame_rate) override {}
     std::vector<u16> ReceiveFrame() override;
     bool CanReceiveFrame() override;
 
