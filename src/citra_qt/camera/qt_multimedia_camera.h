77fec53bac9011225e03f1011d6345d6c858ff15
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-14 15:38:17 -0500

    [skip ci]

--- src/citra_qt/camera/qt_multimedia_camera.h
+++ src/citra_qt/camera/qt_multimedia_camera.h
@@ -9,6 +9,7 @@
 #include <QAbstractVideoSurface>
 #include <QCamera>
 #include <QImage>
+#include <QMutex>
 #include "citra_qt/camera/camera_util.h"
 #include "citra_qt/camera/qt_camera_factory.h"
 #include "core/frontend/camera/interface.h"
@@ -24,10 +25,13 @@ public:
     bool present(const QVideoFrame&) override;
 
 private:
+    QMutex mutex;
     QImage current_frame;
 
     friend class QtMultimediaCamera; // For access to current_frame
 };
+
+class QtMultimediaCameraHandler;
 
 /// This class is only an interface. It just calls QtMultimediaCameraHandler.
 class QtMultimediaCamera final : public CameraInterface {
@@ -44,12 +48,10 @@ public:
     std::vector<u16> ReceiveFrame() override;
     void OnServicePaused() override;
     void OnServiceResumed() override;
-    void OnServiceStopped() override;
     bool IsPreviewAvailable() override;
 
 private:
-    QCamera* camera;
-    QtCameraSurface camera_surface;
+    std::shared_ptr<QtMultimediaCameraHandler> handler;
     int width, height;
     bool output_rgb;
     bool flip_horizontal, flip_vertical;
@@ -66,6 +68,8 @@ class QtMultimediaCameraHandler final : public QObject {
 public:
     /// Creates the global handler. Must be called in UI thread.
     static void Init();
+    static std::shared_ptr<QtMultimediaCameraHandler> GetHandler();
+    static void ReleaseHandler(const std::shared_ptr<QtMultimediaCameraHandler>& handler);
 
     /**
      * Creates the camera.
@@ -83,12 +87,13 @@ public:
     void StopCamera();
     bool CameraAvailable() const;
 
-    /// Pointer to the global QtMultimediaCameraHandler
-    static std::unique_ptr<QtMultimediaCameraHandler> g_qt_multimedia_camera_handler;
-
 private:
+    // QtMultimediaCameraHandler() = default;
     std::unique_ptr<QCamera> camera;
     QtCameraSurface camera_surface{};
+
+    static std::array<std::shared_ptr<QtMultimediaCameraHandler>, 2> handlers;
+    static std::array<bool, 2> status;
 
     friend class QtMultimediaCamera; // For access to camera_surface (and camera)
 };
