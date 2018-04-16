bf8290e169fdc2684e7e3a7516bdb7a8c32f5382
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-16 12:46:16 -0500

    default_ini: change regions to short regions

--- src/citra/default_ini.h
+++ src/citra/default_ini.h
@@ -196,7 +196,7 @@ sd_card_root =
 
 [System]
 # The system region that Citra will use during emulation
-# -1: Auto-select (default), 0: Japan, 1: USA, 2: Europe, 3: Australia, 4: China, 5: Korea, 6: Taiwan
+# -1: Auto-select (default), 0: JPN, 1: USA, 2: EUR, 3: AUS, 4: CHN, 5: KOR, 6: TWN
 region_value =
 
 # Enable New Mode
