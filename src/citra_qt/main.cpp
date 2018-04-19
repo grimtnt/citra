26fa4a7a6f8a8c722080889302b731c41e7f5c5a
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-18 20:09:37 -0500

    citra_qt: implement playing/recording movies with game open; remove playing/recording movies with game not open

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -1044,7 +1044,6 @@ void GMainWindow::OnPauseGame() {
 void GMainWindow::OnStopGame() {
     ShutdownGame();
 
-    Core::Movie::GetInstance().SetPlayFile("");
     Core::Movie::GetInstance().SetRecordFile("");
 }
 
