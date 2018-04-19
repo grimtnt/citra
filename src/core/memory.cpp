60b6d42e63c8b4577ddde7f38e6481f65ba3f694
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-19 12:38:58 -0500

    More minor issue fixes

--- src/core/memory.cpp
+++ src/core/memory.cpp
@@ -312,6 +312,7 @@ u8* GetPhysicalPointer(PAddr address) {
                 break;
             }
         }
+        ASSERT_MSG(target_pointer != nullptr, "Invalid FCRAM address");
         break;
     case N3DS_EXTRA_RAM_PADDR:
         target_pointer = n3ds_extra_ram.data() + offset_into_region;
