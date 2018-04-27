e493e0308073ac60ade22d86b3a5b9da1e927dd0
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-27 15:52:17 -0500

    [skip ci]

--- .travis/linux/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ex
-mkdir -p "$HOME/.ccache"
-docker run -e ENABLE_COMPATIBILITY_REPORTING -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache ubuntu:18.04 /bin/bash -ex /citra/.travis/linux/docker.sh
