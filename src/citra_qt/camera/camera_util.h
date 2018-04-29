a3bab03308a04b9d4567f5b41da4596131ff9ca9
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-04-29 08:38:16 -0500

    [skip ci]

--- src/citra_qt/camera/camera_util.h
+++ src/citra_qt/camera/camera_util.h
@@ -7,9 +7,6 @@
 #include <vector>
 #include "common/common_types.h"
 
-namespace cv {
-class Mat;
-}
 class QImage;
 
 namespace CameraUtil {
@@ -20,10 +17,5 @@ std::vector<u16> Rgb2Yuv(const QImage& source, int width, int height);
 /// Processes the QImage (resizing, flipping ...) and converts it to a std::vector
 std::vector<u16> ProcessImage(const QImage& source, int width, int height, bool output_rgb,
                               bool flip_horizontal, bool flip_vertical);
-
-#ifdef ENABLE_OPENCV_CAMERA
-/// Converts cv::Mat to a std::vector
-QImage cvMat2QImage(const cv::Mat& mat);
-#endif
 
 } // namespace CameraUtil
