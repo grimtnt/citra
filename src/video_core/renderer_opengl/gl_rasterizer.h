8c926d4dffb2880e6cf2651595b5e2fc6a8f2026
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-05-09 10:06:08 -0500

    [skip ci]

--- src/video_core/renderer_opengl/gl_rasterizer.h
+++ src/video_core/renderer_opengl/gl_rasterizer.h
@@ -272,6 +272,7 @@ private:
     size_t uniform_size_aligned_fs;
 
     SamplerInfo texture_cube_sampler;
+    SamplerInfo texture_shadow_sampler;
 
     OGLBuffer lighting_lut_buffer;
     OGLTexture lighting_lut;
