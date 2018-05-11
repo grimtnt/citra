6fd332dad74a12fe92c0caf04663723d10528a0d
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-05-11 14:48:26 -0500

    [skip ci]

--- src/citra_qt/configuration/configure_graphics.cpp
+++ src/citra_qt/configuration/configure_graphics.cpp
@@ -18,12 +18,8 @@ ConfigureGraphics::ConfigureGraphics(QWidget* parent)
     ui->frame_limit->setEnabled(Settings::values.use_frame_limit);
     connect(ui->toggle_frame_limit, &QCheckBox::stateChanged, ui->frame_limit,
             &QSpinBox::setEnabled);
-    ui->toggle_bos->setEnabled(!Core::System::GetInstance().IsPoweredOn());
 
     ui->layoutBox->setEnabled(!Settings::values.custom_layout);
-
-    connect(ui->renderer_comboBox, SIGNAL(currentIndexChanged(int)), this,
-            SLOT(UpdateRenderer(int)));
 
     connect(ui->layout_bg, &QPushButton::clicked, this, [this] {
         QColor new_color = QColorDialog::getColor(bg_color, this);
@@ -33,66 +29,56 @@ ConfigureGraphics::ConfigureGraphics(QWidget* parent)
                 QString("QPushButton { background-color: %1 }").arg(bg_color.name()));
         }
     });
+
+    ui->hw_renderer_group->setEnabled(ui->toggle_hw_renderer->isChecked());
+    connect(ui->toggle_hw_renderer, &QCheckBox::stateChanged, ui->hw_renderer_group,
+            &QWidget::setEnabled);
+    ui->hw_shader_group->setEnabled(ui->toggle_hw_shader->isChecked());
+    connect(ui->toggle_hw_shader, &QCheckBox::stateChanged, ui->hw_shader_group,
+            &QWidget::setEnabled);
 }
 
 ConfigureGraphics::~ConfigureGraphics() {}
 
 void ConfigureGraphics::setConfiguration() {
-    ui->renderer_comboBox->setCurrentIndex(static_cast<int>(Settings::values.use_hw_renderer));
-    ui->hw_shaders->setCurrentIndex(static_cast<int>(Settings::values.hw_shaders));
-    ui->accurate_hardware_shader->setChecked(Settings::values.shaders_accurate_mul);
-    ui->software_shader_jit->setChecked(Settings::values.use_shader_jit);
-    ui->opengl_shader_jit->setChecked(Settings::values.use_shader_jit);
+    ui->toggle_hw_renderer->setChecked(Settings::values.use_hw_renderer);
+    ui->toggle_hw_shader->setChecked(Settings::values.use_hw_shader);
+    ui->toggle_accurate_gs->setChecked(Settings::values.shaders_accurate_gs);
+    ui->toggle_accurate_mul->setChecked(Settings::values.shaders_accurate_mul);
+    ui->toggle_shader_jit->setChecked(Settings::values.use_shader_jit);
     ui->resolution_factor_combobox->setCurrentIndex(Settings::values.resolution_factor);
     ui->toggle_vsync->setChecked(Settings::values.use_vsync);
     ui->toggle_frame_limit->setChecked(Settings::values.use_frame_limit);
+    ui->frame_limit->setValue(Settings::values.frame_limit);
+    ui->layout_combobox->setCurrentIndex(static_cast<int>(Settings::values.layout_option));
+    ui->swap_screen->setChecked(Settings::values.swap_screen);
     bg_color.setRgbF(Settings::values.bg_red, Settings::values.bg_green, Settings::values.bg_blue);
     ui->layout_bg->setStyleSheet(
         QString("QPushButton { background-color: %1 }").arg(bg_color.name()));
     ui->toggle_bos->setChecked(Settings::values.use_bos);
-    ui->skip_flush_region->setChecked(Settings ::values.skip_flush_region);
-    ui->frame_limit->setValue(Settings::values.frame_limit);
-    ui->layout_combobox->setCurrentIndex(static_cast<int>(Settings::values.layout_option));
-    ui->swap_screen->setChecked(Settings::values.swap_screen);
-
-    UpdateRenderer(static_cast<int>(Settings::values.use_hw_renderer));
+    ui->skip_flush_region->setChecked(Settings::values.skip_flush_region);
 }
 
 void ConfigureGraphics::applyConfiguration() {
-    Settings::values.use_hw_renderer = ui->renderer_comboBox->currentIndex() != 0;
-    Settings::values.hw_shaders = static_cast<Settings::HwShaders>(ui->hw_shaders->currentIndex());
-    Settings::values.shaders_accurate_mul = ui->accurate_hardware_shader->isChecked();
+    Settings::values.use_hw_renderer = ui->toggle_hw_renderer->isChecked();
+    Settings::values.use_hw_shader = ui->toggle_hw_shader->isChecked();
+    Settings::values.shaders_accurate_gs = ui->toggle_accurate_gs->isChecked();
+    Settings::values.shaders_accurate_mul = ui->toggle_accurate_mul->isChecked();
+    Settings::values.use_shader_jit = ui->toggle_shader_jit->isChecked();
     Settings::values.resolution_factor =
         static_cast<u16>(ui->resolution_factor_combobox->currentIndex());
     Settings::values.use_vsync = ui->toggle_vsync->isChecked();
     Settings::values.use_frame_limit = ui->toggle_frame_limit->isChecked();
+    Settings::values.frame_limit = ui->frame_limit->value();
     Settings::values.bg_red = bg_color.redF();
     Settings::values.bg_green = bg_color.greenF();
     Settings::values.bg_blue = bg_color.blueF();
-    Settings::values.use_bos = ui->toggle_bos->isChecked();
     Settings::values.skip_flush_region = ui->skip_flush_region->isChecked();
-    Settings::values.frame_limit = ui->frame_limit->value();
+    Settings::values.use_bos = ui->toggle_bos->isChecked();
     Settings::values.layout_option =
         static_cast<Settings::LayoutOption>(ui->layout_combobox->currentIndex());
     Settings::values.swap_screen = ui->swap_screen->isChecked();
-
-    if (Settings::values.use_hw_renderer) {
-        Settings::values.use_shader_jit = ui->opengl_shader_jit->isChecked();
-    } else {
-        Settings::values.use_shader_jit = ui->software_shader_jit->isChecked();
-    }
-
     Settings::Apply();
-}
-
-void ConfigureGraphics::UpdateRenderer(int val) {
-    if (Settings::values.use_hw_renderer) {
-        ui->opengl_render_group->show();
-        ui->software_render_group->hide();
-    } else {
-        ui->opengl_render_group->hide();
-        ui->software_render_group->show();
-    }
 }
 
 void ConfigureGraphics::retranslateUi() {
