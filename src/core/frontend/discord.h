cb5cf34a415a0df3d3675e0e47f527f4182d1da5
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-05-30 00:16:54 -0500

    [skip ci]

--- src/core/frontend/discord.h
+++ src/core/frontend/discord.h
@@ -4,63 +4,12 @@
 
 #pragma once
 
-#include <ctime>
 #include <string>
-#include <discord_rpc.h>
-#include "common/common_types.h"
 
-static s64 start_time;
-static bool send_presence = true;
+namespace DiscordRPC {
 
-static void updateDiscordPresence(const std::string& title) {
-    if (send_presence) {
-        DiscordRichPresence discordPresence{};
-        discordPresence.state = title.empty() ? "Unknown game" : title.c_str();
-        discordPresence.details = "Playing";
-        discordPresence.startTimestamp = start_time;
-        discordPresence.largeImageKey = "citra-logo";
-        Discord_UpdatePresence(&discordPresence);
-    } else {
-        Discord_ClearPresence();
-    }
-}
+void Init();
+void Shutdown();
+void Update(const std::string& title);
 
-static void handleDiscordReady(const DiscordUser* request) {}
-
-static void handleDiscordDisconnected(int errorCode, const char* message) {}
-
-static void handleDiscordError(int errorCode, const char* message) {}
-
-static void handleDiscordJoin(const char* joinSecret) {}
-
-static void handleDiscordSpectate(const char* spectateSecret) {}
-
-static void handleDiscordJoinRequest(const DiscordUser* request) {
-    Discord_Respond(request->userId, DISCORD_REPLY_NO);
-}
-
-static void discordInit() {
-    start_time = time(NULL);
-    send_presence = true;
-    DiscordEventHandlers handlers{};
-    handlers.ready = handleDiscordReady;
-    handlers.disconnected = handleDiscordDisconnected;
-    handlers.errored = handleDiscordError;
-    handlers.joinGame = handleDiscordJoin;
-    handlers.spectateGame = handleDiscordSpectate;
-    handlers.joinRequest = handleDiscordJoinRequest;
-    Discord_Initialize("432553040755556374", &handlers, 1, NULL);
-}
-
-static void discordLoop() {
-#ifdef DISCORD_DISABLE_IO_THREAD
-    Discord_UpdateConnection();
-#endif
-    Discord_RunCallbacks();
-}
-
-static void discordShutdown() {
-    send_presence = false;
-    Discord_ClearPresence();
-    Discord_Shutdown();
-}
+} // namespace DiscordRPC
