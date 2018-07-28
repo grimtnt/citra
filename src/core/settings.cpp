445067c1376c54f5d00de8c69bfd0ebbb65d668d
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-07-27 23:32:59 -0500

    [skip ci]

--- src/core/settings.cpp
+++ src/core/settings.cpp
@@ -33,6 +33,50 @@ void Apply() {
 
     Service::HID::ReloadInputDevices();
     Service::IR::ReloadInputDevices();
+    Service::CAM::ReloadCameraDevices();
+}
+
+template <typename T>
+void LogSetting(const std::string& name, const T& value) {
+    LOG_INFO(Config, "{}: {}", name, value);
+}
+
+void LogSettings() {
+    LOG_INFO(Config, "Citra Configuration:");
+    LogSetting("Core_UseCpuJit", Settings::values.use_cpu_jit);
+    LogSetting("Core_SwkbdImplementation", Settings::values.swkbd_implementation);
+    LogSetting("Renderer_UseHwRenderer", Settings::values.use_hw_renderer);
+    LogSetting("Renderer_UseHwShader", Settings::values.use_hw_shader);
+    LogSetting("Renderer_ShadersAccurateGs", Settings::values.shaders_accurate_gs);
+    LogSetting("Renderer_ShadersAccurateMul", Settings::values.shaders_accurate_mul);
+    LogSetting("Renderer_UseShaderJit", Settings::values.use_shader_jit);
+    LogSetting("Renderer_UseResolutionFactor", Settings::values.resolution_factor);
+    LogSetting("Renderer_UseVsync", Settings::values.use_vsync);
+    LogSetting("Renderer_UseBos", Settings::values.use_bos);
+    LogSetting("Renderer_UseFrameLimit", Settings::values.use_frame_limit);
+    LogSetting("Renderer_FrameLimit", Settings::values.frame_limit);
+    LogSetting("Layout_Toggle3d", Settings::values.toggle_3d);
+    LogSetting("Layout_Factor3d", Settings::values.factor_3d);
+    LogSetting("Layout_LayoutOption", static_cast<int>(Settings::values.layout_option));
+    LogSetting("Layout_SwapScreen", Settings::values.swap_screen);
+    LogSetting("Audio_OutputEngine", Settings::values.sink_id);
+    LogSetting("Audio_EnableAudioStretching", Settings::values.enable_audio_stretching);
+    LogSetting("Audio_OutputDevice", Settings::values.audio_device_id);
+    using namespace Service::CAM;
+    LogSetting("Camera_OuterRightName", Settings::values.camera_name[OuterRightCamera]);
+    LogSetting("Camera_OuterRightConfig", Settings::values.camera_config[OuterRightCamera]);
+    LogSetting("Camera_OuterRightFlip", Settings::values.camera_flip[OuterRightCamera]);
+    LogSetting("Camera_InnerName", Settings::values.camera_name[InnerCamera]);
+    LogSetting("Camera_InnerConfig", Settings::values.camera_config[InnerCamera]);
+    LogSetting("Camera_InnerFlip", Settings::values.camera_flip[InnerCamera]);
+    LogSetting("Camera_OuterLeftName", Settings::values.camera_name[OuterLeftCamera]);
+    LogSetting("Camera_OuterLeftConfig", Settings::values.camera_config[OuterLeftCamera]);
+    LogSetting("Camera_OuterLeftFlip", Settings::values.camera_flip[OuterLeftCamera]);
+    LogSetting("DataStorage_UseVirtualSd", Settings::values.use_virtual_sd);
+    LogSetting("System_EnableNewMode", Settings::values.enable_new_mode);
+    LogSetting("System_RegionValue", Settings::values.region_value);
+    LogSetting("Hacks_PriorityBoost", Settings::values.priority_boost);
+    LogSetting("Hacks_CpuJit", Settings::values.cpu_jit_hacks);
 }
 
 } // namespace Settings
