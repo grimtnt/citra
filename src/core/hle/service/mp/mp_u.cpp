5941e8907274ccfad6e6c8d00810576c3c586102
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-08-14 17:33:22 -0500

    remove tabs

--- src/core/hle/service/mp/mp_u.cpp
+++ src/core/hle/service/mp/mp_u.cpp
@@ -9,16 +9,10 @@ namespace MP {
 
 MP_U::MP_U() : ServiceFramework("mp:u", DefaultMaxSessions) {
     const FunctionInfo functions[] = {
-        // clang-format off
-		{0x00010040, nullptr, "GetHandle"},
-        {0x00020000, nullptr, "Initialize"},
-        {0x00030000, nullptr, "Shutdown"},
-        {0x000400C0, nullptr, "Bind"},
-        {0x00050042, nullptr, "SetBeaconData"},
-		{0x00070042, nullptr, "SetAppData"},
-		{0x00110080, nullptr, "RecvDataFrame"},
-		{0x001400C2, nullptr, "SendDataFrame"},
-        // clang-format on
+        {0x00010040, nullptr, "GetHandle"},     {0x00020000, nullptr, "Initialize"},
+        {0x00030000, nullptr, "Shutdown"},      {0x000400C0, nullptr, "Bind"},
+        {0x00050042, nullptr, "SetBeaconData"}, {0x00070042, nullptr, "SetAppData"},
+        {0x00110080, nullptr, "RecvDataFrame"}, {0x001400C2, nullptr, "SendDataFrame"},
     };
     RegisterHandlers(functions);
 }
