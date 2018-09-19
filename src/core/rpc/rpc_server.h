4073c906bfcbe92493daab7916ea3d3aaac9e528
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-09-19 13:35:11 -0500

    minor change

--- src/core/rpc/rpc_server.h
+++ src/core/rpc/rpc_server.h
@@ -29,7 +29,6 @@ private:
     void HandleTouchState(Packet& packet, s16 x, s16 y, bool valid);
     void HandleMotionState(Packet& packet, s16 x, s16 y, s16 z, s16 roll, s16 pitch, s16 yaw);
     void HandleCircleState(Packet& packet, s16 x, s16 y);
-    void HandleSetResolution(Packet& packet, u16 resolution);
     void HandleSetGame(Packet& packet, const std::string&);
     void HandleSetOverrideControls(Packet& packet, bool pad, bool touch, bool motion, bool circle);
     void HandlePause(Packet& packet);
