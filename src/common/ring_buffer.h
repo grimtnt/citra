26ae78690f39c460dfd67ed5a0852ae0bd8e4ad2
MerryMage <MerryMage@users.noreply.github.com>
2018-09-19 07:14:36 +0100

    ring_buffer: Fix Push

--- src/common/ring_buffer.h
+++ src/common/ring_buffer.h
@@ -36,14 +36,14 @@ public:
     /// @param new_slots   Pointer to the slots to push
     /// @param slot_count  Number of slots to push
     /// @returns The number of slots actually pushed
-    size_t Push(const void* new_slots, size_t slot_count) {
-        const size_t write_index{m_write_index.load()};
-        const size_t slots_free{capacity + m_read_index.load() - write_index};
-        const size_t push_count{std::min(slot_count, slots_free)};
+    std::size_t Push(const void* new_slots, std::size_t slot_count) {
+        const std::size_t write_index{m_write_index.load()};
+        const std::size_t slots_free{capacity + m_read_index.load() - write_index};
+        const std::size_t push_count{std::min(slot_count, slots_free)};
 
-        const size_t pos{write_index % capacity};
-        const size_t first_copy{std::min(capacity - pos, push_count)};
-        const size_t second_copy{push_count - first_copy};
+        const std::size_t pos{write_index % capacity};
+        const std::size_t first_copy{std::min(capacity - pos, push_count)};
+        const std::size_t second_copy{push_count - first_copy};
 
         const char* in{static_cast<const char*>(new_slots)};
         std::memcpy(m_data.data() + pos * granularity, in, first_copy * slot_size);
@@ -55,22 +55,22 @@ public:
         return push_count;
     }
 
-    size_t Push(const std::vector<T>& input) {
-        return Push(input.data(), input.size());
+    std::size_t Push(const std::vector<T>& input) {
+        return Push(input.data(), input.size() / granularity);
     }
 
     /// Pops slots from the ring buffer
     /// @param output     Where to store the popped slots
     /// @param max_slots  Maximum number of slots to pop
     /// @returns The number of slots actually popped
-    size_t Pop(void* output, size_t max_slots = ~size_t(0)) {
-        const size_t read_index{m_read_index.load()};
-        const size_t slots_filled{m_write_index.load() - read_index};
-        const size_t pop_count{std::min(slots_filled, max_slots)};
+    std::size_t Pop(void* output, size_t max_slots = ~std::size_t(0)) {
+        const std::size_t read_index{m_read_index.load()};
+        const std::size_t slots_filled{m_write_index.load() - read_index};
+        const std::size_t pop_count{std::min(slots_filled, max_slots)};
 
-        const size_t pos{read_index % capacity};
-        const size_t first_copy{std::min(capacity - pos, pop_count)};
-        const size_t second_copy{pop_count - first_copy};
+        const std::size_t pos{read_index % capacity};
+        const std::size_t first_copy{std::min(capacity - pos, pop_count)};
+        const std::size_t second_copy{pop_count - first_copy};
 
         char* out{static_cast<char*>(output)};
         std::memcpy(out, m_data.data() + pos * granularity, first_copy * slot_size);
@@ -82,28 +82,28 @@ public:
         return pop_count;
     }
 
-    std::vector<T> Pop(size_t max_slots = ~size_t(0)) {
+    std::vector<T> Pop(std::size_t max_slots = ~std::size_t(0)) {
         std::vector<T> out(std::min(max_slots, capacity) * granularity);
-        const size_t count{Pop(out.data(), out.size() / granularity)};
+        const std::size_t count{Pop(out.data(), out.size() / granularity)};
         out.resize(count * granularity);
         return out;
     }
 
     /// @returns Number of slots used
-    size_t Size() const {
+    std::size_t Size() const {
         return m_write_index.load() - m_read_index.load();
     }
 
     /// @returns Maximum size of ring buffer
-    constexpr size_t Capacity() const {
+    constexpr std::size_t Capacity() const {
         return capacity;
     }
 
 private:
     // It is important to align the below variables for performance reasons:
     // Having them on the same cache-line would result in false-sharing between them.
-    alignas(128) std::atomic<size_t> m_read_index{0};
-    alignas(128) std::atomic<size_t> m_write_index{0};
+    alignas(128) std::atomic<std::size_t> m_read_index{0};
+    alignas(128) std::atomic<std::size_t> m_write_index{0};
 
     std::array<T, granularity * capacity> m_data;
 };
