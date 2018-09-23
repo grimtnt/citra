83cdedae4bf040165db926cb31ce480123bd2093
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-09-22 22:30:33 -0500

    minor changes

--- src/common/string_util.cpp
+++ src/common/string_util.cpp
@@ -162,11 +162,11 @@ std::string UTF16ToUTF8(const std::u16string& input) {
 std::u16string UTF8ToUTF16(const std::string& input) {
 #ifdef _MSC_VER
     // Workaround for missing char16_t/char32_t instantiations in MSVC2017
-    std::wstring_convert<std::codecvt_utf8_utf16<__int16>, __int16> convert{};
+    std::wstring_convert<std::codecvt_utf8_utf16<__int16>, __int16> convert;
     auto tmp_buffer{convert.from_bytes(input)};
     return std::u16string(tmp_buffer.cbegin(), tmp_buffer.cend());
 #else
-    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert{};
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
     return convert.from_bytes(input);
 #endif
 }
