yuzu-emu-mirror/yuzu

thread: Convert ThreadStatus into an enum class
Makes the thread status strongly typed, so implicit conversions can't
happen. It also makes it easier to catch mistakes at compile time.

src/core/hle/kernel/address_arbiter.cpp
@@ -20,7 +20,7 @@ namespace AddressArbiter {
 static ResultCode WaitForAddress(VAddr address, s64 timeout) {
     SharedPtr<Thread> current_thread = GetCurrentThread();
     current_thread->arb_wait_address = address;
-    current_thread->status = THREADSTATUS_WAIT_ARB;
+    current_thread->status = ThreadStatus::WaitArb;
     current_thread->wakeup_callback = nullptr;
 
     current_thread->WakeAfterDelay(timeout);
@@ -65,7 +65,7 @@ static void WakeThreads(std::vector<SharedPtr<Thread>>& waiting_threads, s32 num
 
     // Signal the waiting threads.
     for (size_t i = 0; i < last; i++) {
-        ASSERT(waiting_threads[i]->status == THREADSTATUS_WAIT_ARB);
+        ASSERT(waiting_threads[i]->status == ThreadStatus::WaitArb);
         waiting_threads[i]->SetWaitSynchronizationResult(RESULT_SUCCESS);
         waiting_threads[i]->arb_wait_address = 0;
         waiting_threads[i]->ResumeFromWait();

src/core/hle/kernel/svc.cpp
@@ -133,7 +133,7 @@ static ResultCode GetProcessId(u32* process_id, Handle process_handle) {
 /// Default thread wakeup callback for WaitSynchronization
 static bool DefaultThreadWakeupCallback(ThreadWakeupReason reason, SharedPtr<Thread> thread,
                                         SharedPtr<WaitObject> object, size_t index) {
-    ASSERT(thread->status == THREADSTATUS_WAIT_SYNCH_ANY);
+    ASSERT(thread->status == ThreadStatus::WaitSynchAny);
 
     if (reason == ThreadWakeupReason::Timeout) {
         thread->SetWaitSynchronizationResult(RESULT_TIMEOUT);
@@ -197,7 +197,7 @@ static ResultCode WaitSynchronization(Handle* index, VAddr handles_address, u64 
         object->AddWaitingThread(thread);
 
     thread->wait_objects = std::move(objects);
-    thread->status = THREADSTATUS_WAIT_SYNCH_ANY;
+    thread->status = ThreadStatus::WaitSynchAny;
 
     // Create an event to wake the thread up after the specified nanosecond delay has passed
     thread->WakeAfterDelay(nano_seconds);
@@ -217,7 +217,7 @@ static ResultCode CancelSynchronization(Handle thread_handle) {
         return ERR_INVALID_HANDLE;
     }
 
-    ASSERT(thread->status == THREADSTATUS_WAIT_SYNCH_ANY);
+    ASSERT(thread->status == ThreadStatus::WaitSynchAny);
     thread->SetWaitSynchronizationResult(
         ResultCode(ErrorModule::Kernel, ErrCodes::SynchronizationCanceled));
     thread->ResumeFromWait();
@@ -468,8 +468,8 @@ static void ExitProcess() {
                 continue;
 
             // TODO(Subv): When are the other running/ready threads terminated?
-            ASSERT_MSG(thread->status == THREADSTATUS_WAIT_SYNCH_ANY ||
-                           thread->status == THREADSTATUS_WAIT_SYNCH_ALL,
+            ASSERT_MSG(thread->status == ThreadStatus::WaitSynchAny ||
+                           thread->status == ThreadStatus::WaitSynchAll,
                        "Exiting processes with non-waiting threads is currently unimplemented");
 
             thread->Stop();
@@ -545,7 +545,7 @@ static ResultCode StartThread(Handle thread_handle) {
         return ERR_INVALID_HANDLE;
     }
 
-    ASSERT(thread->status == THREADSTATUS_DORMANT);
+    ASSERT(thread->status == ThreadStatus::Dormant);
 
     thread->ResumeFromWait();
     Core::System::GetInstance().CpuCore(thread->processor_id).PrepareReschedule();
@@ -596,7 +596,7 @@ static ResultCode WaitProcessWideKeyAtomic(VAddr mutex_addr, VAddr condition_var
     current_thread->condvar_wait_address = condition_variable_addr;
     current_thread->mutex_wait_address = mutex_addr;
     current_thread->wait_handle = thread_handle;
-    current_thread->status = THREADSTATUS_WAIT_MUTEX;
+    current_thread->status = ThreadStatus::WaitMutex;
     current_thread->wakeup_callback = nullptr;
 
     current_thread->WakeAfterDelay(nano_seconds);
@@ -656,7 +656,7 @@ static ResultCode SignalProcessWideKey(VAddr condition_variable_addr, s32 target
         if (mutex_val == 0) {
             // We were able to acquire the mutex, resume this thread.
             Memory::Write32(thread->mutex_wait_address, thread->wait_handle);
-            ASSERT(thread->status == THREADSTATUS_WAIT_MUTEX);
+            ASSERT(thread->status == ThreadStatus::WaitMutex);
             thread->ResumeFromWait();
 
             auto lock_owner = thread->lock_owner;
@@ -672,8 +672,8 @@ static ResultCode SignalProcessWideKey(VAddr condition_variable_addr, s32 target
             Handle owner_handle = static_cast<Handle>(mutex_val & Mutex::MutexOwnerMask);
             auto owner = g_handle_table.Get<Thread>(owner_handle);
             ASSERT(owner);
-            ASSERT(thread->status != THREADSTATUS_RUNNING);
-            thread->status = THREADSTATUS_WAIT_MUTEX;
+            ASSERT(thread->status != ThreadStatus::Running);
+            thread->status = ThreadStatus::WaitMutex;
             thread->wakeup_callback = nullptr;
 
             // Signal that the mutex now has a waiting thread.

grimtnt/citra

src/core/hle/kernel/address_arbiter.cpp
@@ -14,7 +14,7 @@ namespace Kernel {
 
 void AddressArbiter::WaitThread(SharedPtr<Thread> thread, VAddr wait_address) {
     thread->wait_address = wait_address;
-    thread->status = THREADSTATUS_WAIT_ARB;
+    thread->status = ThreadStatus::WaitArb;
     waiting_threads.emplace_back(std::move(thread));
 }
 
@@ -22,7 +22,7 @@ void AddressArbiter::ResumeAllThreads(VAddr address) {
     // Determine which threads are waiting on this address, those should be woken up.
     auto itr{std::stable_partition(waiting_threads.begin(), waiting_threads.end(),
                                    [address](const auto& thread) {
-                                       ASSERT_MSG(thread->status == THREADSTATUS_WAIT_ARB,
+                                       ASSERT_MSG(thread->status == ThreadStatus::WaitArb,
                                                   "Inconsistent AddressArbiter state");
                                        return thread->wait_address != address;
                                    })};
@@ -38,7 +38,7 @@ SharedPtr<Thread> AddressArbiter::ResumeHighestPriorityThread(VAddr address) {
     // Determine which threads are waiting on this address, those should be considered for wakeup.
     auto matches_start{std::stable_partition(waiting_threads.begin(), waiting_threads.end(),
                                              [address](const auto& thread) {
-                                                 ASSERT_MSG(thread->status == THREADSTATUS_WAIT_ARB,
+                                                 ASSERT_MSG(thread->status == ThreadStatus::WaitArb,
                                                             "Inconsistent AddressArbiter state");
                                                  return thread->wait_address != address;
                                              })};

src/core/hle/kernel/svc.cpp
@@ -236,8 +236,8 @@ static void ExitProcess() {
             continue;
 
         // TODO(Subv): When are the other running/ready threads terminated?
-        ASSERT_MSG(thread->status == THREADSTATUS_WAIT_SYNCH_ANY ||
-                       thread->status == THREADSTATUS_WAIT_SYNCH_ALL,
+        ASSERT_MSG(thread->status == ThreadStatus::WaitSynchAny ||
+                       thread->status == ThreadStatus::WaitSynchAll,
                    "Exiting processes with non-waiting threads is currently unimplemented");
 
         thread->Stop();
@@ -379,14 +379,14 @@ static ResultCode WaitSynchronization1(Handle handle, s64 nano_seconds) {
 
         thread->wait_objects = {object};
         object->AddWaitingThread(thread);
-        thread->status = THREADSTATUS_WAIT_SYNCH_ANY;
+        thread->status = ThreadStatus::WaitSynchAny;
 
         // Create an event to wake the thread up after the specified nanosecond delay has passed
         thread->WakeAfterDelay(nano_seconds);
 
         thread->wakeup_callback = [](ThreadWakeupReason reason, SharedPtr<Thread> thread,
                                      SharedPtr<WaitObject> object) {
-            ASSERT(thread->status == THREADSTATUS_WAIT_SYNCH_ANY);
+            ASSERT(thread->status == ThreadStatus::WaitSynchAny);
 
             if (reason == ThreadWakeupReason::Timeout) {
                 thread->SetWaitSynchronizationResult(RESULT_TIMEOUT);
@@ -461,7 +461,7 @@ static ResultCode WaitSynchronizationN(s32* out, VAddr handles_address, s32 hand
             return RESULT_TIMEOUT;
 
         // Put the thread to sleep
-        thread->status = THREADSTATUS_WAIT_SYNCH_ALL;
+        thread->status = ThreadStatus::WaitSynchAll;
 
         // Add the thread to each of the objects' waiting threads.
         for (auto& object : objects) {
@@ -475,7 +475,7 @@ static ResultCode WaitSynchronizationN(s32* out, VAddr handles_address, s32 hand
 
         thread->wakeup_callback = [](ThreadWakeupReason reason, SharedPtr<Thread> thread,
                                      SharedPtr<WaitObject> object) {
-            ASSERT(thread->status == THREADSTATUS_WAIT_SYNCH_ALL);
+            ASSERT(thread->status == ThreadStatus::WaitSynchAll);
 
             if (reason == ThreadWakeupReason::Timeout) {
                 thread->SetWaitSynchronizationResult(RESULT_TIMEOUT);
@@ -517,7 +517,7 @@ static ResultCode WaitSynchronizationN(s32* out, VAddr handles_address, s32 hand
             return RESULT_TIMEOUT;
 
         // Put the thread to sleep
-        thread->status = THREADSTATUS_WAIT_SYNCH_ANY;
+        thread->status = ThreadStatus::WaitSynchAny;
 
         // Add the thread to each of the objects' waiting threads.
         for (std::size_t i{}; i < objects.size(); ++i) {
@@ -535,7 +535,7 @@ static ResultCode WaitSynchronizationN(s32* out, VAddr handles_address, s32 hand
 
         thread->wakeup_callback = [](ThreadWakeupReason reason, SharedPtr<Thread> thread,
                                      SharedPtr<WaitObject> object) {
-            ASSERT(thread->status == THREADSTATUS_WAIT_SYNCH_ANY);
+            ASSERT(thread->status == ThreadStatus::WaitSynchAny);
 
             if (reason == ThreadWakeupReason::Timeout) {
                 thread->SetWaitSynchronizationResult(RESULT_TIMEOUT);
@@ -674,7 +674,7 @@ static ResultCode ReplyAndReceive(s32* index, VAddr handles_address, s32 handle_
     // No objects were ready to be acquired, prepare to suspend the thread.
 
     // Put the thread to sleep
-    thread->status = THREADSTATUS_WAIT_SYNCH_ANY;
+    thread->status = ThreadStatus::WaitSynchAny;
 
     // Add the thread to each of the objects' waiting threads.
     for (std::size_t i{}; i < objects.size(); ++i) {
@@ -686,7 +686,7 @@ static ResultCode ReplyAndReceive(s32* index, VAddr handles_address, s32 handle_
 
     thread->wakeup_callback = [](ThreadWakeupReason reason, SharedPtr<Thread> thread,
                                  SharedPtr<WaitObject> object) {
-        ASSERT(thread->status == THREADSTATUS_WAIT_SYNCH_ANY);
+        ASSERT(thread->status == ThreadStatus::WaitSynchAny);
         ASSERT(reason == ThreadWakeupReason::Signal);
 
         ResultCode result{RESULT_SUCCESS};